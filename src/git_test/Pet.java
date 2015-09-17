c6449a0a4a7a812d7965ecb632bf683035b0bff2
Kahyelee <kahye5232@naver.com>
2015-09-17 13:18:58 +0900

    Test

--- src/git_test/Pet.java
+++ src/git_test/Pet.java
@@ -1,5 +1,61 @@
 package git_test;
 
 public class Pet {
-
+	private String name;
+	private int age; // in year
+	private double weight; //in pounds
+	
+        // 오버라이딩
+	public String toString() {
+		return ("Name: " + name + " Age: " + age + " years"
+				+ "\nWeight: " + weight + " pounds");
+	}
+	
+	public Pet(String name, int initialAge, double initialWeight) {
+		 this.name = name;
+	}
+        // 오버로딩
+	public Pet(String initialName) {
+		name = initialName;
+		age = 0;
+		weight = 0;
+	}
+	
+	public Pet( int initialAge) {
+		name = "No name yet.";
+		weight = 0;
+		if (initialAge < 0) {
+			System.out.println("Error: Negative age.");
+			System.exit(0);
+		} else
+			age = initialAge;
+	}
+	public Pet() {
+		name = "No name yet.";
+		age = 0;
+		weight = 0;
+	}
+	public void set(String newName, int newAge, double newWeight) {
+		
+	}
+	
+	public void setAge( int newAge) {
+               this.age = newAge;
+	}
+	
+	public void setWeight( double newWeight) {
+	      this.weight = newWeight;
+       }
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public double getWeight() {
+		return weight;
+	}
 }
